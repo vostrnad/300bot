59270033f033af1c6af0f94ebcc64eb86f019aa7
Miroito <alban.vachette@gmail.com>
2021-08-06 21:52:35 +0200

    Second commit for hi.ts

--- src/modules/commands/common/hi.ts
+++ src/modules/commands/common/hi.ts
@@ -5,7 +5,7 @@ import { isDigit } from '@app/validators/string'
 export default new Command({
   keyword: 'hi',
   description: 'Says hi!',
-  help: 'Usage: `{prefix}hi <number of sides>` - says hi!',
+  help: 'Usage: `{prefix}hi` - says hi!',
   callback: ({ args, reply, env }) => {
     if (args.length === 0) {
       return reply('Hi!')
