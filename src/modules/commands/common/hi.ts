e02d9757c1d48b094346c7130f30fb0bf356c6c2
Miroito <alban.vachette@gmail.com>
2021-08-06 21:57:44 +0200

    3rd version

--- src/modules/commands/common/hi.ts
+++ src/modules/commands/common/hi.ts
@@ -1,6 +1,4 @@
 import { Command } from '@commands/CommandHandler'
-import { randomBigInt } from '@app/utils/random'
-import { isDigit } from '@app/validators/string'
 
 export default new Command({
   keyword: 'hi',
