528ad118d14673e848e8d8f0e67dfeefaa6a8234
Miroito <alban.vachette@gmail.com>
2021-08-06 21:45:24 +0200

    Hi this is a git commit message

---
+++ src/modules/commands/common/hi.ts
@@ -0,0 +1,14 @@
+import { Command } from '@commands/CommandHandler'
+import { randomBigInt } from '@app/utils/random'
+import { isDigit } from '@app/validators/string'
+
+export default new Command({
+  keyword: 'hi',
+  description: 'Says hi!',
+  help: 'Usage: `{prefix}hi <number of sides>` - says hi!',
+  callback: ({ args, reply, env }) => {
+    if (args.length === 0) {
+      return reply('Hi!')
+    }
+  },
+})
