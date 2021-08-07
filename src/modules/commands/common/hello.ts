6f70689160e36cbabaf9aa9bffcda511ce5253b7
Miroito <alban.vachette@gmail.com>
2021-08-07 13:25:40 +0200

    Merge branch 'hi-command' of https://github.com/Miroito/300bot into hi-command

--- src/modules/commands/common/hello.ts
+++ src/modules/commands/common/hello.ts
@@ -1,11 +1,12 @@
 import { Command } from '@commands/CommandHandler'
 
+//Hello command responding with the name of the person
 export default new Command({
   keyword: 'hello',
   description: 'say hello',
   help: 'Usage: `{prefix}hi` - say hello',
-  callback: async ({ args, reply, author}) => {
-  if (args.length > 0) return
-  reply(`Hello ${author.displayName}!`)
+  callback: ({ args, reply, author }) => {
+    if (args.length > 0) return
+    reply(`Hello ${author.displayName}!`)
   },
 })
