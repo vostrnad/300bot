aaf83bb1c99d220f6eb8e320c40e2751300f7c57
Vojtech Strnad <vojta.strnad@gmail.com>
2021-08-08 00:17:25 +0200

    Refactor CensusApi

---
+++ src/modules/planetside/types.ts
@@ -0,0 +1,52 @@
+export type Character = {
+  characterId: string
+  name: {
+    first: string
+    firstLower: string
+  }
+  factionId: string
+  titleId: string
+  times: {
+    creation: string
+    lastLogin: string
+    minutesPlayed: string
+  }
+  battleRank: {
+    value: string
+  }
+  prestigeLevel: string
+}
+
+export type CharactersOnlineStatus = {
+  characterId: string
+  onlineStatus: string
+}
+
+export type Outfit = {
+  outfitId: string
+  name: string
+  nameLower: string
+  alias: string
+  aliasLower: string
+  timeCreated: string
+  leaderCharacterId: string
+  memberCount: string
+}
+
+export type OutfitMember = {
+  outfitId: string
+  characterId: string
+  memberSince: string
+  rank: string
+  rankOrdinal: string
+}
+
+export type Title = {
+  titleId: string
+  name: { en: string }
+}
+
+export type World = {
+  worldId: string
+  name: { en: string }
+}
