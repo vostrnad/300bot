f7c43415419217fe9c3199655d3bc314abf715e6
Miroito <88556823+Miroito@users.noreply.github.com>
2021-08-07 13:51:26 +0200

    Add a hello command (#7)

---
+++ src/modules/commands/common/hello.ts
@@ -0,0 +1,12 @@
+import { Command } from '@commands/CommandHandler'
+
+//Hello command responding with the name of the person
+export default new Command({
+  keyword: 'hello',
+  description: 'say hello',
+  help: 'Usage: `{prefix}hi` - say hello',
+  callback: ({ args, reply, author }) => {
+    if (args.length > 0) return
+    reply(`Hello ${author.displayName}!`)
+  },
+})
