cc045c6b43dad178c9ea11e0e819c7f7c379b6d6
Miroito <88556823+Miroito@users.noreply.github.com>
2021-08-07 17:44:27 +0200

    Add role support to russianroulette command (#11)

--- src/modules/commands/discord/russianroulette.ts
+++ src/modules/commands/discord/russianroulette.ts
@@ -1,15 +1,25 @@
+import discord from 'discord.js'
 import { Command } from '@commands/CommandHandler'
 import { randomBigInt, randomChoice } from '@app/utils/random'
+import { sleep } from '@app/utils/async'
 
-export default new Command({
+export default new Command<discord.Message>({
   keyword: 'russianroulette',
   description: 'kill yourself for fun purposes',
   help: 'Usage: `{prefix}russianroulette` - kill yourself for fun purposes',
-  callback: ({ args, reply, author }) => {
+  callback: async ({ args, author, reply, raw }) => {
     if (args.length > 0) return
     const rounds = 6
     const rolled = randomBigInt(BigInt(rounds)) + BigInt(1)
+    const deadRole = raw.guild?.roles.cache.find((role) => role.name === 'Dead')
+
+    if (!(deadRole instanceof discord.Role)) {
+      return reply('The Dead role is not defined.')
+    }
+
     if (rolled === BigInt(1)) {
+      await raw.member?.roles.add(deadRole)
+
       reply(
         `**${author.displayName}** ` +
           randomChoice([
@@ -19,6 +29,8 @@ export default new Command({
             'now has a sixth hole in their head.',
           ]),
       )
+      await sleep(10 * 1000)
+      await raw.member?.roles.remove(deadRole)
     } else {
       reply(
         `**${author.displayName}** ` +
