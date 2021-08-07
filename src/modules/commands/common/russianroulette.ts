a88bf76c685cc0bdc7555aab33836a996c0ab774
Miroito <alban.vachette@gmail.com>
2021-08-07 15:40:38 +0200

    Rusian roulette with role

--- src/modules/commands/common/russianroulette.ts
+++ src/modules/commands/common/russianroulette.ts
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import discord from 'discord.js'
 import { Command } from '@commands/CommandHandler'
 import { randomBigInt, randomChoice } from '@app/utils/random'
 import { sleep } from '@app/utils/async'
-=======
-import { Command } from '@commands/CommandHandler'
-import { randomBigInt, randomChoice } from '@app/utils/random'
->>>>>>> 32ea10d136ec20c60804cd080ad3cba10fcdea21
 
 export default new Command({
   keyword: 'russianroulette',
   description: 'kill yourself for fun purposes',
   help: 'Usage: `{prefix}russianroulette` - kill yourself for fun purposes',
-<<<<<<< HEAD
   callback: ({ args, reply, author, raw }) => {
     if (args.length > 0) return
 
@@ -24,13 +18,6 @@ export default new Command({
 
     if (rolled === BigInt(1)) {
       raw.author.roles.add(Deadrole)
-=======
-  callback: ({ args, reply, author }) => {
-    if (args.length > 0) return
-    const rounds = 6
-    const rolled = randomBigInt(BigInt(rounds)) + BigInt(1)
-    if (rolled === BigInt(1)) {
->>>>>>> 32ea10d136ec20c60804cd080ad3cba10fcdea21
       reply(
         `**${author.displayName}** ` +
           randomChoice([
@@ -40,11 +27,8 @@ export default new Command({
             'now has a sixth hole in their head.',
           ]),
       )
-<<<<<<< HEAD
       sleep(10 * 1000)
       raw.author.roles.remove(Deadrole)
-=======
->>>>>>> 32ea10d136ec20c60804cd080ad3cba10fcdea21
     } else {
       reply(
         `**${author.displayName}** ` +
