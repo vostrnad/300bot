9d8069bbb25075e9b3497163cf03c206d26faaf5
Miroito <88556823+Miroito@users.noreply.github.com>
2021-08-07 14:09:15 +0200

    Fix help text of hello command (#8)

--- src/modules/commands/common/hello.ts
+++ src/modules/commands/common/hello.ts
@@ -4,7 +4,7 @@ import { Command } from '@commands/CommandHandler'
 export default new Command({
   keyword: 'hello',
   description: 'say hello',
-  help: 'Usage: `{prefix}hi` - say hello',
+  help: 'Usage: `{prefix}hello` - say hello',
   callback: ({ args, reply, author }) => {
     if (args.length > 0) return
     reply(`Hello ${author.displayName}!`)
