9eb442665ca17107b366da28da16861145ac413b
Vojtěch Strnad <43024885+vostrnad@users.noreply.github.com>
2021-09-05 14:55:15 +0200

    Add isbruoffline as alias

--- src/modules/commands/common/isbruonline.ts
+++ src/modules/commands/common/isbruonline.ts
@@ -4,9 +4,10 @@ import { censusApi } from '@planetside/CensusApi'
 
 export default new Command({
   keyword: 'isbruonline',
+  alias: ['isbruoffline'],
   description: 'check if Bru is online',
   help: 'Usage: `{prefix}isbruonline` - checks if Bru is online',
-  callback: async ({ args, reply }) => {
+  callback: async ({ args, alias, reply }) => {
     if (args.length > 0) return
 
     const status = await censusApi.getCharactersOnlineStatus(
@@ -16,6 +17,10 @@ export default new Command({
     if (status === null) {
       return reply('Bru has deleted this character.')
     }
-    return reply(status ? 'Yes, Bru is online!' : 'No, Bru is offline.')
+    if (alias === 'isbruonline') {
+      return reply(status ? 'Yes, Bru is online!' : 'No, Bru is offline.')
+    } else {
+      return reply(status ? 'No, Bru is online.' : 'Yes, Bru is offline!')
+    }
   },
 })
