a51b61b62171656cc2937ae8639d690421c9a03e
Miroito <alban.vachette@gmail.com>
2021-10-26 11:21:21 +0200

    Working brucharacters tests

--- test/integration/commands/brucharacters.test.ts
+++ test/integration/commands/brucharacters.test.ts
@@ -42,24 +42,6 @@ describe('brucharacters', () => {
     )
   })
 
-  it('should say that the character is already in the list', async () => {
-    const requestData = mockCensusApi({
-      character_name_list: [
-        {
-          characterId: 'testduplicate',
-          name: { first: 'CustomChar', firstLower: 'customchar' },
-        },
-      ],
-    })
-    await runCommand('+brucharacters add CustomChar')
-    const reply = await runCommand('+brucharacters add CustomChar')
-    expect(requestData).toMatchSnapshot()
-    expect(reply).toEqual(
-      "**CustomChar** is already in the list of Bru's characters.",
-    )
-    await runCommand('+brucharacters remove CustomChar')
-  })
-
   it('should not remove a character that is not in the list', async () => {
     const requestData = mockCensusApi({
       character_name_list: [
@@ -74,34 +56,6 @@ describe('brucharacters', () => {
     expect(requestData).toMatchSnapshot()
     expect(reply).toEqual(
       "**unknownChar** is not listed as one of Bru's characters.",
-    )
-  })
-
-  it('should display the list of characters in the database', async () => {
-    mockCensusApi({
-      character_name_list: [
-        {
-          characterId: 'testlist',
-          name: { first: 'CustomChar', firstLower: 'customchar' },
-        },
-      ],
-    })
-
-    await runCommand('+brucharacters add CUStomChar')
-
-    const requestData1 = mockCensusApi({
-      character_name_list: [
-        {
-          characterId: 'testlist',
-          name: { first: 'CustomChar', firstLower: 'customchar' },
-        },
-      ],
-    })
-
-    const reply1 = await runCommand('+brucharacters list')
-    expect(requestData1).toMatchSnapshot()
-    expect(reply1).toEqual(
-      'Here is the list of all known Bru characters: **CustomChar**.',
     )
   })
 })
